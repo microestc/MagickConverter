6586115c18f454de34055c6ed3e4252a85dc53e1
microestc <microestc@outlook.com>
2019-08-29 17:38:57 +0800

    update

--- MagickConverter/MagickService.cs
+++ MagickConverter/MagickService.cs
@@ -15,6 +15,7 @@ namespace MagickConverter
         public void Convert(TaskSetup setup)
         {
             var task = new MagickTask(setup);
+            task.TaskFinishedCallback += () => TaskFinished(setup);
             task.ConvertFinishedEvent += ConvertFinished;
             task.ConvertProgressEvent += ConvertProgress;
             _queue.EnqueueTask(task);
@@ -23,15 +24,20 @@ namespace MagickConverter
         public virtual void ConvertFinished(object sender, ConvertFinishedEventArgs args)
         {
             var setup = args.Setup;
-            setup.CompletedCallBack?.Invoke(setup);
-            if (!string.IsNullOrWhiteSpace(setup.Source))
-                File.Delete(setup.Source);
         }
 
         public virtual void ConvertProgress(object sender, ConvertProgressEventArgs args)
         {
             //if (MagickTask.ProgressCompleted(sender, args, out var setup)) { }
             //if (MagickTask.GainedDuration(sender, args, out var setup)) { }
+        }
+
+        public virtual void TaskFinished(TaskSetup setup)
+        {
+            if (!string.IsNullOrWhiteSpace(setup.Source))
+                File.Delete(setup.Source);
+            if (setup.CompletedCallBack != null)
+                setup.CompletedCallBack(setup);
         }
     }
 }
